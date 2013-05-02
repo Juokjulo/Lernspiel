04a2d397b811092c4e4a642060b7ebc03e8514f9
Juokjulo <elisabeth.mansel@web.de>
2013-05-02 02:39:21 +0200

    name changed

--- js/loaddata.js
+++ js/loaddata.js
@@ -3,7 +3,7 @@
 	this.wrong = wrong;
 
 };
-game.username = "Karl";
+game.username = "Elli";
 var word1 = new word("kenne", "kene");
 var word2 = new word("lesen", "lessen");
 game.words = [word1,word2];
