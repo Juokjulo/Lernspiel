f01696262e218652780fbb77db6c11f14ca91a19
Juokjulo <elisabeth.mansel@web.de>
2013-05-16 16:46:46 +0200

    more refactoring

--- js/interaction.js
+++ js/interaction.js
@@ -1,15 +1,9 @@
 
 game.fight = function fight(teammate) {
-    var background = game.getImage("fight");
-    var font = new me.Font("Tahoma", 18, "#000000");
-
     
     me.state.set(me.state.Info, new game.InteractionScreen(teammate));
     me.state.change(me.state.Info);
-   //  var fight_box = new game.FightObject();
-     
-    //me.game.add(fight_box);
-    //me.game.sort.defer(game.sort);
+   
 }
 
 game.InteractionScreen = me.ScreenObject.extend(
